85922d13387d3eacfa988da44dbc5a3c576af472
Adrián <Adrin@192.168.1.38>
2017-12-28 10:40:13 +0000

    Version 3

--- src/pa04/model/Histogram.java
+++ src/pa04/model/Histogram.java
@@ -23,3 +23,61 @@ public class Histogram<T> {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
+package pa04.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Histogram<T> {
+    private final Map<T, Integer> map = new HashMap<>();
+    
+    public Integer get(T key){
+        return map.get(key);
+    }
+    
+    public Set<T> keySet() {
+        return map.keySet();
+    }
+    
+    public void increment(T key){
+        map.put(key, map.containsKey(key) ? map.get(key) + 1 : 1);
+    }
+
+    public Number getKey() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    Map<T, Integer> map = new HashMap<>();
+    
+    public Integer get(T key){
+        return map.get(key);
+    }
+    
+    public Set<T> keySet() {
+        return map.keySet();
+    }
+    
+    public void increment(T key){
+        map.put(key, map.containsKey(key) ? map.get(key) + 1 : 1);
+    }
+}
+package pa04.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Histogram<T> {
+    Map<T, Integer> map = new HashMap<>();
+    
+    public Integer get(T key){
+        return map.get(key);
+    }
+    
+    public Set<T> keySet() {
+        return map.keySet();
+    }
+    
+    public void increment(T key){
+        map.put(key, map.containsKey(key) ? map.get(key) + 1 : 1);
+    }
+}
--- src/pa04/model/Mail.java
+++ src/pa04/model/Mail.java
@@ -19,3 +19,58 @@ public class Mail {
     }
     
 }
+package pa04.model;
+
+public class Mail {
+    
+    private final String mail;
+    private final int id;
+
+    public Mail(String mail, int id) {
+        this.mail = mail;
+        this.id = id;
+    }
+
+    public String getMail() {
+        return mail;
+    }  
+
+    public int getId() {
+        return id;
+    }
+    
+
+    public Mail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getMail() {
+        return mail;
+    }  
+    
+    public String getDomain(){
+        String[] dom = mail.split("@");
+        return dom[1];
+    }
+
+}
+package pa04.model;
+
+public class Mail {
+    
+    private final String mail;
+
+    public Mail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getMail() {
+        return mail;
+    }  
+    
+    public String getDomain(){
+        String[] dom = mail.split("@");
+        return dom[1];
+    }
+
+}
